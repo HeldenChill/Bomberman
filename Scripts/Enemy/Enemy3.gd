d996c8987526387c314c743441948af0abb75d5d
dangnamlss <dangnamlss@gmail.com>
2020-11-30 00:16:15 +0700

    Update Enemy3.gd

--- Scripts/Enemy/Enemy3.gd
+++ Scripts/Enemy/Enemy3.gd
@@ -6,69 +6,61 @@ var rayLeft : RayCast2D
 var rayUp : RayCast2D
 var rayDown : RayCast2D
 
-func _init(var _heath = 50, var _speed = 80) . (_heath, _speed) :
+func _init(var _heath = 50, var _speed = 20) . (_heath, _speed) :
 	faceRight = true
 	direction = 1
 	pass
 	
 
+func _ready():
+	rayRight = $RayRight;
+	rayLeft = $RayLeft;
+	rayDown = $RayDown;
+	rayUp = $RayUp;
+	$Timer.start(1);
+	velocity = Vector2.ZERO
+	pass
+	
 func _physics_process(delta):
 	move_and_slide(move())
+	
 	
 func random()->int:
 	var x : int
 	randomize()
-	x = randi() %3 + 1
+	x = randi() % 4
 	return x
+	pass
+
+func chooseDirect()->Vector2:
+	
+	if(choice == 0):
+		velocity = Vector2(1,0)
+	elif(choice == 1):
+		velocity = Vector2(-1,0)
+	elif(choice == 2):
+		velocity = Vector2(0,1)
+	else:
+		velocity = Vector2(0, -1)
+	return velocity
 	pass
 	
 func move()->Vector2 :
-	if(rayRight.get_collider()):
-		choice = random()
-		if(choice == 1):
-			velocity.x = -speed
-			velocity.y = 0
-		elif(choice == 2):
-			velocity.y = -speed
-			velocity.x = 0
-		elif(choice == 3):
-			velocity.y = speed
-			velocity.x = 0
-			
-	if(rayLeft.get_collider()):
-		choice = random()
-		if(choice == 1):
-			velocity.x = speed
-			velocity.y = 0
-		elif(choice == 2):
-			velocity.y = -speed
-			velocity.x = 0
-		elif(choice == 3):
-			velocity.y = speed
-			velocity.x = 0
-	
-	if(rayUp.get_collider()):
-		choice = random()
-		if(choice == 1):
-			velocity.x = -speed
-			velocity.y = 0
-		elif(choice == 2):
-			velocity.y = 0
-			velocity.x = speed
-		elif(choice == 3):
-			velocity.y = speed
-			velocity.x = 0
-			
-	if(rayDown.get_collider()):
-		choice = random()
-		if(choice == 1):
-			velocity.x = -speed
-			velocity.y = 0
-		elif(choice == 2):
-			velocity.y = 0
-			velocity.x = speed
-		elif(choice == 3):
-			velocity.y = -speed
-			velocity.x = 0
-	return velocity
+	if(is_on_wall()):
+		var oldChoice = choice
+		while(oldChoice == choice):
+			choice = random()
+		chooseDirect()
+	if((int(global_position.x) - 16) % 32 == 0):
+		if((int(global_position.y) - 16) % 32 == 0):
+			choice = random()
+			chooseDirect()
+			print("ok")
+	return velocity * speed
 	pass
+
+
+func _on_Timer_timeout():
+	choice = random()
+	chooseDirect()
+	pass # Replace with function body.
