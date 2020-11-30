f81c0b917114e1250bdef4316159bd0c912dc9ce
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2020-11-30 20:36:17 +0700

    Add files via upload

---
+++ Scripts/Control/GUIGame/GUIGame.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	var canvasTranform = get_viewport().canvas_transform;
+	transform = canvasTranform;
+	pass
