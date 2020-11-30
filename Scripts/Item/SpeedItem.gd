b9f6c93df3a205ae82480c0b714146637a9140b8
dangnamlss <dangnamlss@gmail.com>
2020-12-01 00:59:28 +0700

    Update SpeedItem.gd

--- Scripts/Item/SpeedItem.gd
+++ Scripts/Item/SpeedItem.gd
@@ -1,7 +1,7 @@
 extends AbstractItem
 
-
 func _on_Area2D_body_entered(body):
-	print("SPEED UP")
+	var player = get_tree().get_root().get_child(0).get_node("Player")
+	player.setSpeed(player.getSpeed() + 20)
 	queue_free()
 	pass # Replace with function body.
